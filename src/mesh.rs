032159d2f74d6c146149318004122ce9ffacdfce
3c1u (Hikaru Terazono) <298748+3c1u@users.noreply.github.com>
2019-07-20 23:11:29 +0900

    Experimental implementation of textured mesh

--- src/mesh.rs
+++ src/mesh.rs
@@ -2,19 +2,84 @@
 
 use math::Matrix2d;
 use triangulation;
-use types::{Color, Rectangle, SourceRectangle};
+use types::{Color, SourceRectangle};
 use DrawState;
 use Graphics;
 use ImageSize;
 
-#[derive(Copy, Clone)]
+use std::sync::Arc;
+
+/// A textured mesh.
+#[derive(Clone)]
 pub struct Mesh {
     /// The color
     pub color: Option<Color>,
     /// The image source rectangle
     pub source_rectangle: Option<SourceRectangle>,
+
+    /// The UV coordinates.
+    pub uvs: Option<Arc<[[f32; 2]]>>,
+    /// The vertices.
+    pub vertices: Option<Arc<[[f32; 2]]>>,
 }
 
-impl Mesh {}
+impl Mesh {
+    /// Creates a textured mesh.
+    pub fn new() -> Mesh {
+        Mesh {
+            color: None,
+            source_rectangle: None,
+            uvs: None,
+            vertices: None,
+        }
+    }
 
-impl Default for Mesh {}
+    /// Draws a textured mesh.
+    pub fn draw<G>(
+        &self,
+        texture: &<G as Graphics>::Texture,
+        draw_state: &DrawState,
+        transform: Matrix2d,
+        g: &mut G,
+    ) where
+        G: Graphics,
+    {
+        use math::Scalar;
+
+        let color = self.color.unwrap_or([1.0; 4]);
+
+        let source_rectangle = self.source_rectangle.unwrap_or({
+            let (w, h) = texture.get_size();
+            [0.0, 0.0, w as Scalar, h as Scalar]
+        });
+
+        let rectangle = [
+            0.0,
+            0.0,
+            source_rectangle[2] as Scalar,
+            source_rectangle[3] as Scalar,
+        ];
+
+        let uvs = self
+            .uvs
+            .clone()
+            .unwrap_or(Arc::new(triangulation::rect_tri_list_uv(
+                texture,
+                source_rectangle,
+            )));
+        let vertices = self
+            .vertices
+            .clone()
+            .unwrap_or(Arc::new(triangulation::rect_tri_list_xy(
+                transform, rectangle,
+            )));
+
+        g.tri_list_uv(draw_state, &color, texture, move |f| f(&uvs, &vertices));
+    }
+}
+
+impl Default for Mesh {
+    fn default() -> Self {
+        Self::new()
+    }
+}
